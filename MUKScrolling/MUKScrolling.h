47a30b305bf90c1bcd112bbcc4767395eb8b2c63
Muccy <muccymac@gmail.com>
2012-04-11 11:39:15 +0200

    First Commit

---
+++ MUKScrolling/MUKScrolling.h
@@ -0,0 +1,1 @@
+#import <MUKScrolling/MUKRecyclingScrollView.h>
---
+++ MUKScrolling/Unit Tests/MUKScrollingTests.h
@@ -0,0 +1,13 @@
+//
+//  MUKScrollingTests.h
+//  MUKScrollingTests
+//
+//  Created by Marco Muccinelli on 11/04/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface MUKScrollingTests : SenTestCase
+
+@end
